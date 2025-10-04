c5e2f47442ceaf9692f27913c8cc61973ce78580
LuisTriguerosE <luisdtrigueros@gmail.com>
2025-10-03 19:59:36 -0600

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,37 @@
+function registrar(){
+const user = document.getElementById("user").value.trim();
+const pin = document.getElementById("pin").value.trim();
+
+if (!user || !pin){
+    alert("debe ingresar correctamente el pin y el usuario");
+    return;
+}
+const usuario = JSON.parse(localStorage.getItem("usuarios")||"{}");
+if(usuarios[user]){
+        alert("El usuario ya existe");
+
+
+    }else{
+
+        usuarios[user] = {pin: pin, saldo:0 };
+        localStorage.setItem("usuarios",JSON.stringify(usuarios));
+        alert("Usuario registrado con exito");
+    }
+}
+function login(){
+const user = document.getElementById("user").value.trim();
+const pin = document.getElementById("pin").value.trim();
+const usuarios = JSON.parse(localStorage.getItem("usuarios")||"{}");
+if (usuarios[user] && usuarios[user].pin===pin ){
+    localStorage.setItem("usuario_actual",JSON.stringify({usuarios: user}));
+    window.location.href="cajero.html";
+}else{
+    alert("credenciales incorrectas");
+}
+
+}
+
+
+
+
+
